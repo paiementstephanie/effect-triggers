b8fd96186913f79e90f5120a198eafd978107004
paiementstephanie <stephanie.paiement@rogers.com>
2016-03-29 16:51:59 -0400

    adding bounce animation

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ var $body = $ ('body');
 $body.on('click', '.btn-show-hide', function () {
   $('.box').toggleClass('is-clicked');
 });
+
+
+$body.on('click', '.btn-bounce', function () {
+  $('.circle').addClass('bounce');
+});
+
+$body.on('animationend', '.circle', function () {
+  $(this).removeClass('bounce');
+});

46f67796c7cfc1cfab85e8701e47d1dbe86af6af
paiementstephanie <stephanie.paiement@rogers.com>
2016-03-29 17:57:56 -0400

    changing to non hyphen classes

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
-var $body = $ ('body');
+var $body = $('body');
+var $show = $('.btn-show-hide');
+var $move = $('btn-move');
+
+
 
 $body.on('click', '.btn-show-hide', function () {
-  $('.box').toggleClass('is-clicked');
+  $('.box').toggleClass('show');
 });
 
 $body.on('click', '.btn-move', function () {
-  $('.diamond').toggleClass('slide-in');
+  $('.diamond').toggleClass('move');
 });
-
-
-
 
 $body.on('click', '.btn-collapse-expand', function () {
   $('.panel').toggleClass('collapse');
 });
-
-
-
 
 $body.on('click', '.btn-bounce', function () {
   $('.circle').addClass('bounce');
@@ -25,7 +23,6 @@ $body.on('click', '.btn-bounce', function () {
 $body.on('animationend', '.circle', function () {
   $(this).removeClass('bounce');
 });
-
 
 $body.on('click', '.btn-append', function () {
   $('.list').append('<li>New List Item</li>');
